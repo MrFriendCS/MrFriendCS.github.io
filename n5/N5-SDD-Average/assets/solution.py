5e7332083c53fc3a1cf83578347e133128c92763
MrFriendCS <esafriend1r@glow.sch.uk>
2024-08-21 15:25:39 +0100

    Added Average task

---
+++ n5/N5-SDD-Average/assets/solution.py
@@ -0,0 +1,39 @@
+# Title: N5 SDD Mean
+# Author: Mr Friend
+# Date: 21 Aug 2024
+
+# Initialise variables
+noOfValues = 0
+value = 0.0
+sum = 0.0
+mean = 0.0
+
+
+# Get number of values
+noOfValues = int(input("How many values? "))
+
+# Display a blank line
+print()
+
+# loop for number of values
+for counter in range(noOfValues):
+    
+    # Get value
+    value = float(input("Value " + str(counter + 1) + ": "))
+    
+    # Add value to sum
+    sum = sum + value
+    
+# Calculate mean
+mean = sum / noOfValues
+
+# Display a blank line
+print()
+
+# Display results
+print("Sum: " + str(sum))
+print("Mean: " + str(mean))
+
+
+
+
---
+++ n5/N5-SDD-Average/instructions.md
@@ -0,0 +1,36 @@
+# N5 SDD - Average
+
+## Introduction
+
+A program is required that will calculate the [mean](https://www.mathsisfun.com/mean.html "Maths help") of a set of numbers.
+
+## Task
+
+Write a short program that will ask the user how many numbers they have.  The program will then ask the user to enter each value in turn.
+
+The sum of the values will be displayed, and the mean.
+
+### Example 1
+
+```
+How many values: 3
+
+Value 1: 5
+Value 2: 15
+Value 3: 10
+
+Sum: 30.0
+Mean: 10.0
+```
+
+### Example 2
+
+```
+How many values: 2
+
+Value 1: 3.7
+Value 2: 8.5
+
+Sum: 12.2
+Mean: 6.1
+```
