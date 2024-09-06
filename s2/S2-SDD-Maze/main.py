4e08107fcbece911da89fc11c5da173c9d7f23be
MrFriendCS <51888864+MrFriendCS@users.noreply.github.com>
2024-09-06 09:16:11 +0100

    Minor changes

---
+++ s2/S2-SDD-Maze/main.py
@@ -0,0 +1,19 @@
+# Setup
+import turtle
+window = turtle.Screen()
+
+# Show a title
+window.title("A maze")
+
+# Select the maze - Other mazes available
+window.bgpic("Maze1.png")
+
+# Get the turtle ready - can rename
+tim = turtle.Turtle()       
+tim.shape("turtle")
+tim.color("red")
+
+# Your code goes below here
+
+
+
