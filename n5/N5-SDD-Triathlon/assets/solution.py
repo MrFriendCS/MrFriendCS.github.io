ada1563722f004c07c166596e77f5e0497a17fd1
MrFriendCS <51888864+MrFriendCS@users.noreply.github.com>
2024-09-06 18:22:04 +0100

    Minor changes

--- n5/N5-SDD-Triathlon/assets/solution.py
+++ n5/N5-SDD-Triathlon/assets/solution.py
@@ -2,26 +2,33 @@
 # Author: Mr Friend
 # Date: 6 Sep 2024
 
-
 # Initialise variables and array
 min = 0
 sec = 0
 stages = ["Swim", "T1", "Cycle", "T2", "Run"]
-total = 0
+totalTime = 0
 
 # Display header
-print("Barra Triathlon")
+print("\nBarra Triathlon")
 print("---------------")
+
+print("\nEnter times for each stage:")
 
 # Loop for each stage
 for index in range(len(stages)):
     
+    # Get valid time - minutes
     min = int(input("\n" + stages[index] + " time (min): "))
     
+    # Get valid time - seconds
     sec = int(input(stages[index] + " time (sec): "))
     
-    total = total + (min * 60 + sec)
-    
+    # Update total time in seconds
+    totalTime = totalTime + (min * 60 + sec)
 
-print("\nTotal: " + str(round(total / 60)) + " mins")
+# Convert total time to minutes - round to nearest minute
+timeMins = round(totalTime / 60)
+
+# Display result
+print("\nTotal: " + str(timeMins) + " mins")
 print("==============")
