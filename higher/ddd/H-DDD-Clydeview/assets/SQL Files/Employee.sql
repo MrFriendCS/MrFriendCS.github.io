d06af3a8ecf9c4e68b130e9c730b53ab2237dd7d
MrFriendCS <51888864+MrFriendCS@users.noreply.github.com>
2024-11-22 23:04:00 +0000

    Updated

---
+++ higher/ddd/H-DDD-Clydeview/assets/SQL Files/Employee.sql
@@ -0,0 +1,23 @@
+CREATE TABLE Employee (
+    employeeID INT NOT NULL,
+    jobTitle VARCHAR(8) NOT NULL,
+    name VARCHAR(20),
+    building VARCHAR(2),
+    yearsEmployeed INT NOT NULL
+        CHECK(yearsEmployeed >= 0),
+    PRIMARY KEY (employeeID)
+);
+
+INSERT INTO Employee VALUES (13428,"Engineer","Becky A.","1e",4);
+INSERT INTO Employee VALUES (13677,"Engineer","Dan B.","1e",2);
+INSERT INTO Employee VALUES (14002,"Engineer","Sharon F.","1e",6);
+INSERT INTO Employee VALUES (14093,"Engineer","Dan M.","1e",4);
+INSERT INTO Employee VALUES (14162,"Engineer","Malcom S.","1e",1);
+INSERT INTO Employee VALUES (14208,"Admin","Tylar S.","2w",2);
+INSERT INTO Employee VALUES (14235,"Admin","Sherman D.","2w",8);
+INSERT INTO Employee VALUES (14689,"Admin","Jakob J.","2w",6);
+INSERT INTO Employee VALUES (15725,"Admin","Lillia A.","2w",7);
+INSERT INTO Employee VALUES (15730,"Admin","Brandon J.","2w",7);
+INSERT INTO Employee VALUES (15992,"Manager","Scott K.","1e",9);
+INSERT INTO Employee VALUES (16432,"Manager","Shirlee M.","1e",3);
+INSERT INTO Employee VALUES (16547,"Manager","Daria O.","2w",6);
---
+++ higher/ddd/H-DDD-Clydeview/assets/SQL Files/Python files/Create Employee Table.py
@@ -0,0 +1,45 @@
+# Title: Employee Table
+# Author: Mr Friend
+# Date: 22 Nov 2024
+
+# Files
+fileIn = open("../CSV files/Employee.csv", "r")
+fileOut = open("../Employee.sql", "w")
+
+
+# Create table
+
+table = """CREATE TABLE Employee (
+    employeeID INT NOT NULL,
+    jobTitle VARCHAR(8) NOT NULL,
+    name VARCHAR(20),
+    building VARCHAR(2),
+    yearsEmployeed INT NOT NULL
+        CHECK(yearsEmployeed >= 0),
+    PRIMARY KEY (employeeID)
+);"""
+
+fileOut.write(table + "\n\n")
+
+
+# Insert data
+
+line = fileIn.readline()  # Ignore heading row
+line = fileIn.readline()
+
+while line != "":
+    
+    data = line.split(",")
+    
+    fileOut.write("INSERT INTO Employee VALUES ")
+    
+    fileOut.write("(" +  data[0].strip() + ",")  # employeeID
+    fileOut.write("\"" + data[1].strip() + "\",")  # jobTitle
+    fileOut.write("\"" + data[2].strip() + "\",")  # name
+    fileOut.write("\"" + data[3].strip() + "\",")  # building
+    fileOut.write(       data[4].strip() + ");\n")  # yearsEmployeed
+
+    line = fileIn.readline()
+    
+fileIn.close()
+fileOut.close()
