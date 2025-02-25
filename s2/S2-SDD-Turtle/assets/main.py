c34dc787d21402e9f7bc83b189e91d77a61afadc
MrFriendCS <51888864+MrFriendCS@users.noreply.github.com>
2025-02-25 19:44:32 +0000

    Updated

--- s2/S2-SDD-Turtle/assets/main.py
+++ s2/S2-SDD-Turtle/assets/main.py
@@ -1,9 +1,25 @@
 # Title: S2 SDD Turtle
-# Author: 
-# Date: 
+# Author: Mr Friend
+# Date: 25 Feb 2025
 
 # Get code to use a turtle
-
+import turtle
 
 # Create a turtle and give it a name
+tim = turtle.Turtle()
+tim.shape("turtle")
+tim.color("red")
 
+# draw outline of house
+tim.goto(150, 0)
+tim.goto(150, 125)
+tim.goto(0, 200)
+
+tim.penup()
+
+tim.goto(-150, 125)
+
+tim.pendown()
+
+tim.goto(-150, 0)
+tim.home()
