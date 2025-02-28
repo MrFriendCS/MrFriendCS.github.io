8f510c4c86a9a8af3503b25a3e69c75840d98597
MrFriendCS <51888864+MrFriendCS@users.noreply.github.com>
2025-02-28 11:38:55 +0000

    Update main.py

--- s2/S2-SDD-Turtle/assets/main.py
+++ s2/S2-SDD-Turtle/assets/main.py
@@ -11,16 +11,18 @@ tim.shape("turtle")
 tim.color("brown")
 
 # Move slowly
-tim.speed(1)
+tim.speed(5)
 
 # Walls
 tim.goto(150, 0)
 tim.goto(150, 125)
 
 # Roof
+tim.begin_fill()
 tim.color("black")
 tim.goto(0, 200)
 tim.goto(-150, 125)
+tim.end_fill()
 
 # Walls
 tim.color("brown")
@@ -28,6 +30,7 @@ tim.goto(-150, 0)
 tim.home()
 
 # Front door
+tim.begin_fill()
 tim.color("blue")
 tim.left(90)
 tim.fd(75)
@@ -35,11 +38,29 @@ tim.right(90)
 tim.fd(50)
 tim.right(90)
 tim.fd(75)
+tim.end_fill()
 
 # Door handle
 tim.pu()
 tim.setheading(135)
 tim.fd(50)
+tim.dot(5, "red")
+
+# Add a flower
+tim.pu()
+tim.goto(50, -150)
+
+# Add stem
+tim.color("pink")
+tim.pd()
+tim.goto(50, -100)
+
+# Add petals
+tim.color("green")
+
+for step in range(5):
+    tim.circle(10)
+    tim.left(360/5)
 
 
     
