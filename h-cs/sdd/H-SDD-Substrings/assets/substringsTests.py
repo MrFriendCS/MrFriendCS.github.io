db1045868d9e8e76e9e52c7ae86d664bdeb3a5a2
MrFriendCS <51888864+MrFriendCS@users.noreply.github.com>
2025-06-17 13:35:53 +0100

    Updated

--- h-cs/sdd/H-SDD-Substrings/assets/substringsTests.py
+++ h-cs/sdd/H-SDD-Substrings/assets/substringsTests.py
@@ -10,16 +10,16 @@ from substrings import *
 def testLeft():
     """Test the funcationality of the left() function."""
     
-    # Initialise variables
+    # Initialise local variables
     failCount = 0
     inputs = ["Hello", "12345", "£1.26p", "Hello", "Hello", "Hello"]
     chars = [4, 3, 5, 0, -1, 6]
     outputs = ["Hell", "123", "£1.26", "Hello", "Hello", "Hello"]
     
-    # Display message
+    # Display function being tested
     print("\nTesting: left() function")
     
-    # Tests
+    # Loop through tests
     for index in range(len(inputs)):
     
         try:
@@ -28,12 +28,14 @@ def testLeft():
             
         except:
             
+            # Increment failure count
             failCount = failCount + 1
             
+            # Display failure message
             print("\tFailed: \"" + inputs[index] + "\", "
                   + str(chars[index]) + " = \"" + outputs[index] + "\"")
     
-    # Display final message
+    # Display success message
     if failCount == 0:
         
         print("\tAll tests passed.")
@@ -42,16 +44,16 @@ def testLeft():
 def testRight():
     """Test the funcationality of the right() function."""
        
-    # Initialise variables
+    # Initialise local variables
     failCount = 0
     inputs = ["Hello", "12345", "£1.26p", "Hello", "Hello", "Hello"]
     chars = [4, 3, 5, 0, -1, 6]
     outputs = ["ello", "345", "1.26p", "Hello", "Hello", "Hello"]
     
-    # Display message
+    # Display function being tested
     print("\nTesting: right() function")
     
-    # Tests
+    # Loop through tests
     for index in range(len(inputs)):
     
         try:
@@ -60,12 +62,14 @@ def testRight():
             
         except:
             
+            # Increment failure count
             failCount = failCount + 1
             
+            # Display failure message
             print("\tFailed: \"" + inputs[index] + "\", "
                   + str(chars[index]) + " = \"" + outputs[index] + "\"")
       
-    # Display final message
+    # Display success message
     if failCount == 0:
         
         print("\tAll tests passed.")
@@ -74,17 +78,17 @@ def testRight():
 def testMid():
     """Test the funcationality of the mid() function."""
     
-    # Initialise variables"Hello!", 2, 3) == "ell"
+    # Initialise local variables"Hello!", 2, 3) == "ell"
     failCount = 0
     inputs = ["Hello", "12345", "£1.26p", "Hello", "Hello", "Hello"]
     starts = [2, 3, 2, 4, -1, 3]
     chars = [3, 2, 3, 0, 3, 5]
     outputs = ["ell", "34", "1.2", "Hello", "Hello", "Hello"]
     
-    # Display message
+    # Display function being tested
     print("\nTesting: mid() function")
     
-    # Tests
+    # Loop through tests
     for index in range(len(inputs)):
     
         try:
@@ -93,13 +97,15 @@ def testMid():
             
         except:
             
+            # Increment failure count
             failCount = failCount + 1
             
+            # Display failure message
             print("\tFailed: \"" + inputs[index] + "\", "
                   + str(starts[index]) + ", " + str(chars[index])
                   + " = \"" + outputs[index] + "\"")
     
-    # Display final message
+    # Display success message
     if failCount == 0:
         
         print("\tAll tests passed.")
