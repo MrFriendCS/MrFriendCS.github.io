f57ed8633d8e59f151e3a1ac3dc5d227e43cffb7
MrFriendCS <51888864+MrFriendCS@users.noreply.github.com>
2025-06-24 13:29:52 +0100

    Updated

--- h-cs/sdd/H-SDD-Standard/assets/standard.py
+++ h-cs/sdd/H-SDD-Standard/assets/standard.py
@@ -29,3 +29,60 @@ def search(values, target):
     
     # Return result
     return found
+
+
+def findMin(values):
+    """Returns the smallest value from an array of values."""
+    
+    # Local variable
+    minimum = values[0]
+    
+    # Loop from second element for each value in array
+    for index in range(1, len(values)):
+        
+        # Check current value is less than current minimum
+        if values[index] < minimum:
+            
+            # Update minimum
+            minimum = values[index]
+    
+    # Return smallest
+    return minimum
+
+
+def findMax(values):
+    """Returns the largest value from an array of values."""
+    
+    # Local variable
+    maximum = values[0]
+    
+    # Loop from second element for each value in array
+    for index in range(1, len(values)):
+        
+        # Check current value is more than current maximum
+        if values[index] > maximum:
+            
+            # Update maximum
+            maximum = values[index]
+    
+    # Return smallest
+    return maximum
+
+
+def count(values, target):
+    """Counts the number of target values in the array of values.  Returns an integer."""
+    
+    # Local variables
+    count = 0
+    
+    # Loop for each value in array
+    for index in range(len(values)):
+        
+        # Check current value and target
+        if values[index] == target:
+            
+            # Increment count
+            count = count + 1
+    
+    # Return result
+    return count
--- h-cs/sdd/H-SDD-Standard/assets/standardTests.py
+++ h-cs/sdd/H-SDD-Standard/assets/standardTests.py
@@ -12,7 +12,7 @@ from standard import *
 def testSearch():
     """Test the functionality of the search() function."""
     
-    # Initialise variable
+    # Initialise local variables
     failCount = 0
     
     values1 = [66.8, 67.4, 83.2, 5.4, 46.8, 60.5]
@@ -20,7 +20,6 @@ def testSearch():
     values3 = ["Campbell", "Ford", "MacDonald", "Smyth", "Young", "Robertson"]
     values4 = [17, 14, 16, 17, 13, 14, 12]
     
-   
     inputs1 = [values1, values2, values3, values4]
     inputs2 = [66.8, "J", "Robertson", 11]
     outputs = [True, True, True, False]
@@ -42,7 +41,7 @@ def testSearch():
             
             # Display failure message
             print("\tFailed Test " + str(index+1) + ": search("
-                  + str(inputs1[index]) + ", \"" + inputs2[index] + "\") = "
+                  + str(inputs1[index]) + ", \"" + str(inputs2[index]) + "\") = "
                   + str(outputs[index]))
     
     # Display success message
@@ -51,6 +50,124 @@ def testSearch():
         print("\tAll " + str(len(inputs1)) + " tests passed.")
 
 
+def testFindMin():
+    """Test the functionality of the findMin() function."""
+    
+    # Initialise local variables
+    failCount = 0
+    
+    values1 = [66.8, 67.4, 83.2, 5.4, 46.8, 60.5]
+    values2 = ["A", "S", "A", "J", "K", "C", "D"]
+    values3 = ["Ford", "MacDonald", "Smyth", "Young", "Robertson", "Campbell"]
+    values4 = [17, 14, 16, 17, 13, 14, 12]
+    
+    inputs = [values1, values2, values3, values4]
+    outputs = [5.4, "A", "Campbell", 12]
+    
+    # Display function being tested
+    print("\nTesting: findMin() function")
+
+    # Loop through tests
+    for index in range(len(inputs)):
+        
+        try:
+            
+            assert findMin(inputs[index]) == outputs[index]
+            
+        except:
+            
+            # Increment failure count
+            failCount = failCount + 1
+            
+            # Display failure message
+            print("\tFailed Test " + str(index+1) + ": findMin("
+                  + str(inputs[index])  + ") = " + str(outputs[index]))
+    
+    # Display success message
+    if failCount == 0:
+        
+        print("\tAll " + str(len(inputs)) + " tests passed.")
+
+
+def testFindMax():
+    """Test the functionality of the findMax() function."""
+    
+    # Initialise local variables
+    failCount = 0
+    
+    values1 = [83.2, 67.4, 66.8, 5.4, 46.8, 60.5]
+    values2 = ["A", "D", "A", "J", "K", "C", "S"]
+    values3 = ["Ford", "MacDonald", "Smyth", "Young", "Robertson", "Campbell"]
+    values4 = [17, 14, 16, 17, 13, 14, 12]
+    
+    inputs = [values1, values2, values3, values4]
+    outputs = [83.2, "S", "Young", 17]
+    
+    # Display function being tested
+    print("\nTesting: findMax() function")
+
+    # Loop through tests
+    for index in range(len(inputs)):
+        
+        try:
+            
+            assert findMax(inputs[index]) == outputs[index]
+            
+        except:
+            
+            # Increment failure count
+            failCount = failCount + 1
+            
+            # Display failure message
+            print("\tFailed Test " + str(index+1) + ": findMax("
+                  + str(inputs[index])  + ") = " + str(outputs[index]))
+    
+    # Display success message
+    if failCount == 0:
+        
+        print("\tAll " + str(len(inputs)) + " tests passed.")
+
+
+def testCount():
+    """Test the functionality of the count() function."""
+    
+    # Initialise local variables
+    failCount = 0
+    
+    values1 = [66.8, 67.4, 83.2, 5.4, 46.8, 60.5]
+    values2 = ["J", "S", "A", "J", "K", "C", "J"]
+    values3 = ["Campbell", "Ford", "MacDonald", "Smyth", "Young", "Robertson"]
+    values4 = [17, 14, 16, 17, 13, 14, 12]
+    
+    inputs1 = [values1, values2, values3, values4]
+    inputs2 = [66.8, "J", "Robertson", 11]
+    outputs = [1, 3, 1, 0]
+    
+    # Display function being tested
+    print("\nTesting: count() function")
+
+    # Loop through tests
+    for index in range(len(inputs1)):
+        
+        try:
+            
+            assert count(inputs1[index], inputs2[index]) == outputs[index]
+            
+        except:
+            
+            # Increment failure count
+            failCount = failCount + 1
+            
+            # Display failure message
+            print("\tFailed Test " + str(index+1) + ": count("
+                  + str(inputs1[index]) + ", \"" + str(inputs2[index]) + "\") = "
+                  + str(outputs[index]))
+    
+    # Display success message
+    if failCount == 0:
+        
+        print("\tAll " + str(len(inputs1)) + " tests passed.")
+
 
 #
 # Main program
@@ -58,3 +175,6 @@ def testSearch():
 
 # Tests
 testSearch()
+testFindMin()
+testFindMax()
+testCount()
