f67198a12ab91056043a14f407cf93f7dce870d9
MrFriendCS <51888864+MrFriendCS@users.noreply.github.com>
2025-11-21 23:16:45 +0000

    Updated

--- n5-cs/sdd/N5-SDD-Chance/assets/solution6.py
+++ n5-cs/sdd/N5-SDD-Chance/assets/solution6.py
@@ -10,64 +10,71 @@ compRPS = 0
 userRPS = ""
 winner = ""
 
+# Display header
+print("Rock Paper Scissors")
+print("-------------------")
+
 # Computer picks RPS
 # 0 = Rock, 1 = Paper, 2 = Scissors
 compRPS = random.randint(0, 2)
 
-print(compRPS)
-
 # User picks RPS
+print()
 userRPS = input("RPS: ")
 
-
+# Loop until user value is valid
 while userRPS != "r" and userRPS != "p" and userRPS != "s":
     
+    # Display error message
     print("Only 'r', 'p', or 's' can be used.")
     
     # User picks RPS
     userRPS = input("RPS: ")
 
+# Blank line
+print()
+
+
+# Decide who one
 if compRPS == 0 and userRPS == "p":
     
     print("Paper wraps rock!")
-    
     winner = "You"
 
 elif compRPS == 0 and userRPS == "s":
     
     print("Rock blunts scissors!")
-    
     winner = "Computer"
 
 elif compRPS == 1 and userRPS == "r":
     
     print("Paper wraps rock!")
-    
     winner = "Computer"
 
 elif compRPS == 1 and userRPS == "s":
     
     print("Scissors cut paper!")
-    
     winner = "You"
 
 elif compRPS == 2 and userRPS == "r":
     
-    print("Rock blunts cissors!")
-    
+    print("Rock blunts scissors!")
     winner = "You"
 
 elif compRPS == 2 and userRPS == "p":
     
     print("Scissors cut paper!")
-    
     winner = "Computer"
     
 else:
     
     print("Draw!")
-    
     winner = "No one"
-    
+
+# Display the winner
+print()
 print("Winner: " + winner)
+
+# Display the footer
+print("===================")
     
---
+++ n5-cs/sdd/N5-SDD-Chance/index6.md
@@ -0,0 +1,53 @@
+# N5 SDD - Chance Part 6
+
+
+## Introduction
+
+Playing against the computer, a player chooses either Rock (`r`), Paper (`p`), or Scissors (`s`).
+
+
+## Task
+
+Implement a solution that matches the design shown below.
+
+
+### Top Level Design (Stucture diagram)
+
+![Structure diagram](assets/sd6.png)
+
+
+## User Interface
+
+Examples of the expected user interface are shown below, with some possible input and output values.
+
+
+### Example 1
+
+```
+Rock Paper Scissors
+-------------------
+
+RPS: r
+
+Paper wraps rock!
+
+Winner: Computer
+
+===================
+```
+
+
+### Example 2
+
+```
+Rock Paper Scissors
+-------------------
+
+RPS: p
+
+Draw!
+
+Winner: No one
+
+===================
+```
