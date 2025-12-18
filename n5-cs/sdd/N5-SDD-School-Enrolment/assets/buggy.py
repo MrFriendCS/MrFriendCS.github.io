7cd4b963c66ae4fa515e92538d944dc440e657db
MrFriendCS <esafriend1r@glow.sch.uk>
2025-12-18 10:30:56 +0000

    Updated

--- n5-cs/sdd/N5-SDD-School-Enrolment/assets/buggy.py
+++ n5-cs/sdd/N5-SDD-School-Enrolment/assets/buggy.py
@@ -7,7 +7,7 @@ age = input("Age: ")
 while age <= 3 and age >= 18:
     print("Invalid!")
     age = str(input("Age: "))
-if age < 5:
+if age <= 5:
     print("Pre-school")
 elif aeg > 5 and age < 12:
     print("Primary")
--- n5-cs/sdd/N5-SDD-School-Enrolment/assets/solution.py
+++ n5-cs/sdd/N5-SDD-School-Enrolment/assets/solution.py
@@ -1,26 +1,46 @@
-# Title: S3 SDD School Enrolment
-# Date: 16 Sep 2023
+# Title: N5 SDD School Enrolment
+# Date: 18 Dec 2025
 # Author: Mr Friend
 
 # Declare variable
 age = 0
+
+# Display header
+print("School Enrolment")
+print("----------------")
+print()
 
 # Get age from user
 age = int(input("Age: "))
 
 # Only accept ages 3 to 18
 while age < 3 or age > 18:
+    
+    # Error message
+    print()
     print("Age must be from 3 to 18")
+    
+    # Get age from user
     age = int(input("Re-enter age: "))
 
 # Pre-school: 3, 4
 if age < 5:
+    
+    # Display school
     print("Pre-school")
 
 # Primary: 5 to 11
 elif age < 12:
+    
+    # Display school
     print("Primary")
 
 # Secondary: 12 to 18
 else:
+    
+    # Display school
     print("Secondary")
+
+# Display footer
+print()
+print("================")
