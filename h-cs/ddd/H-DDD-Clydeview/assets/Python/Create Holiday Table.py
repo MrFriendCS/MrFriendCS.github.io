b3079bd6ab93b4404931284c0df2ab00b95e5fdd
MrFriendCS <esafriend1r@glow.sch.uk>
2026-03-16 10:30:55 +0000

    Updated

---
+++ h-cs/ddd/H-DDD-Clydeview/assets/Python/Create Holiday Table.py
@@ -0,0 +1,49 @@
+# Title: Holiday Table
+# Author: Mr Friend
+# Date: 13 Dec 2024
+
+# Files
+fileIn = open("../CSV/Hotel-Holiday/Holiday.csv", "r")
+fileOut = open("../SQL/Holiday.sql", "w")
+
+
+# Create table
+
+table = """CREATE TABLE Holiday (
+    name VARCHAR(30) NOT NULL,
+    destination VARCHAR(30) NOT NULL,
+    country VARCHAR(20),
+    dateOfDeparture DATE NOT NULL,
+    nights INT,
+    hotelRef VARCHAR(4),
+    FOREIGN KEY (hotelRef)
+        REFERENCES Hotel(hotelRef),
+    PRIMARY KEY (title)
+);"""
+
+fileOut.write(table + "\n\n")
+
+
+# Insert data
+
+line = fileIn.readline()  # Ignore heading row
+line = fileIn.readline()
+
+while line != "":
+    
+    data = line.split(",")
+    
+    fileOut.write("INSERT INTO Holiday VALUES (")
+    
+    fileOut.write("\"" + data[0].strip() + "\",")  # title
+    fileOut.write("\"" + data[1].strip() + "\",")  # destination
+    fileOut.write("\"" + data[2].strip() + "\",")  # country
+    fileOut.write("\"" + data[3].strip() + "\",")  # dateOfDeparture
+    fileOut.write("\"" + data[4].strip() + "\",")  # nights
+    fileOut.write("\"" + data[5].strip() + "\");")  # hotelRef
+    fileOut.write("\n")
+
+    line = fileIn.readline()
+    
+fileIn.close()
+fileOut.close()
