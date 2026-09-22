982efe2dc218297aed6613001db16bcbf012f489
MrFriendCS <esafriend1r@glow.sch.uk>
2026-09-22 13:37:48 +0100

    Update find_country.py

--- ah-cs/sdd/AH-SDD-Standard/assets/find_country.py
+++ ah-cs/sdd/AH-SDD-Standard/assets/find_country.py
@@ -2,19 +2,40 @@
 # author: Mr Friend
 # Date: 22 Sep 2026
 
-def read_data() -> tuple(list[str], list[str], list[real], list[int]):
+def read_data() -> tuple[list[str], list[str], list[float], list[int]]:
     """Read data from csv file and return parallel arrays."""
     
     # Initialise local variables
     countries: list[str] = ["" for _ in range(47)]
-    captilas: list[str] = ["" for _ in range(47)]
+    capitals: list[str] = ["" for _ in range(47)]
     areas: list[float] = [0.0 for _ in range(47)]
     populations: list[int] = [0 for _ in range(47)]
-    data: list[str] = ["" for _ in range(3)]
+    data: list[str] = ["" for _ in range(4)]
     line: str = ""
     
     # Connect to file
     file = open('countries.csv', 'r', encoding='utf-8')
+    
+    # Read first line and discard headers
+    file.readline()
+    
+    # Loop for each record
+    for index in range(len(countries)):
+        
+        # Read line
+        line = file.readline()
+        
+        # Seperate values
+        data = line.split(',')
+        
+        # Assign values to parallel arrays
+        countries[index] = data[0].strip()
+        capitals[index] = data[1].strip()
+        areas[index] = float(data[2].strip())
+        populations[index] = int(data[3].strip())
+        
+    # Return parallel arrays
+    return countries, capitals, areas, populations
 
 
 def find_country(countries: list[str], country: str) -> int:
@@ -30,7 +51,7 @@ def write_summary() -> None:
 
 
 def main() -> None:
-    pass
+    read_data()
 
 
 # Run program
