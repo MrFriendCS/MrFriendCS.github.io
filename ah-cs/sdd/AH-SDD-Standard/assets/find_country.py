5187e67211f5a829122d43a773633bc94eba304b
MrFriendCS <esafriend1r@glow.sch.uk>
2026-09-22 13:24:34 +0100

    Updated

---
+++ ah-cs/sdd/AH-SDD-Standard/assets/find_country.py
@@ -0,0 +1,39 @@
+# Title: AH SDD Find a Country
+# author: Mr Friend
+# Date: 22 Sep 2026
+
+def read_data() -> tuple(list[str], list[str], list[real], list[int]):
+    """Read data from csv file and return parallel arrays."""
+    
+    # Initialise local variables
+    countries: list[str] = ["" for _ in range(47)]
+    captilas: list[str] = ["" for _ in range(47)]
+    areas: list[float] = [0.0 for _ in range(47)]
+    populations: list[int] = [0 for _ in range(47)]
+    data: list[str] = ["" for _ in range(3)]
+    line: str = ""
+    
+    # Connect to file
+    file = open('countries.csv', 'r', encoding='utf-8')
+
+
+def find_country(countries: list[str], country: str) -> int:
+    pass
+
+
+def display_country() -> None:
+    pass
+
+
+def write_summary() -> None:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+# Run program
+if __name__ == "__main__":
+
+    main()
